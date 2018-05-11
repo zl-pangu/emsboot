a73db32f003cf7dd7a4d8d204ca2ffbee9238c51
zl-pangu <18380428197@163.com>
2018-05-11 17:51:58 +0800

    删除无效的文件

--- boot/src/main/java/com/zhph/service/common/LoginService.java
+++
@@ -1,7 +0,0 @@
-package com.zhph.service.common;
-
-import javax.servlet.http.HttpServletRequest;
-
-public interface LoginService {
-	public void doLogin(HttpServletRequest req, String userName, String pwd) throws Exception;
-}
